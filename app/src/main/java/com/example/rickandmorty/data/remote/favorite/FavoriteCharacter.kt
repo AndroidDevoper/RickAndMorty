ecf09c80bb7259b856a8db10d15058b87a10c26c
serg <serg.vip.haker@gmail.com>
2024-08-02 14:47:53 +0300

    Favorites

---
+++ app/src/main/java/com/example/rickandmorty/data/remote/favorite/FavoriteCharacter.kt
@@ -0,0 +1,22 @@
+package com.example.rickandmorty.data.remote.favorite
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorite_characters")
+data class FavoriteCharacter(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val originName: String,
+    val originUrl: String,
+    val locationName: String,
+    val locationUrl: String,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
